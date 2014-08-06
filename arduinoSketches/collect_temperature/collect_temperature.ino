ed8b3c5749a8c98fe0818a03ed90dd38ea5a7242
Hector Garcia <hectorgarcia@gmail.com>
2014-08-06 13:59:39 +0900

    Read new temperature from DHT11

--- arduinoSketches/collect_temperature/collect_temperature.ino
+++ arduinoSketches/collect_temperature/collect_temperature.ino
@@ -1,16 +1,20 @@
 /*
-Chibi Arduino using a DHT11 temperature sensor 
-and broadcasting the latest temperature reading
+Freakduino Chibi-9000 using a DHT11 temperature sensor 
+and broadcasting the latest temperature reading. 
+It sends only the temperature.
 */
 
 #include <chibi.h>
 #include "DHT.h"
 
+#define DHTTYPE DHT11   // Type of DHT sensor, in our case we are using DHT11
 #define DHT11_PIN A0    // Pin where the DHT11 is connected
-#define DHTTYPE DHT11
 
 dht DHT;
 
+/**************************************************************************/
+// Initialize
+/**************************************************************************/
 void setup()
 {  
   // Initialize the chibi command line and set the speed to 57600 bps
@@ -22,18 +26,43 @@ void setup()
   Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");
 }
 
+/**************************************************************************/
+// Loop
+/**************************************************************************/
 void loop()
 {
-  // DHT Data
+  // Read latest temperature data from DHT11
+  int chk = DHT.read11(DHT11_PIN);
+  switch (chk)
+  {
+    case DHTLIB_OK:  
+		Serial.print("OK,\t"); 
+		break;
+    case DHTLIB_ERROR_CHECKSUM: 
+		Serial.print("Checksum error,\t"); 
+		break;
+    case DHTLIB_ERROR_TIMEOUT: 
+		Serial.print("Time out error,\t"); 
+		break;
+    default: 
+		Serial.print("Unknown error,\t"); 
+		break;
+  }
+
+  // Print the DHT Data
   Serial.print(DHT.humidity, 1);
   Serial.print(",\t");
   Serial.println(DHT.temperature, 1);
 
-  // Read data and transfer it to a buffer
+  // Transfer the temperature to a buffer
   byte temperature_buffer[10]; 
-  dtostrf(DHT.temperature,2,2,(char *)temperature_buffer);
+
+  //AVR libc function to convert from double to char* , sprintf doesn't work
+  dtostrf(DHT.temperature,2,2, (char *)temperature_buffer); 
   
   // Send the data to other Chibis
   chibiTx(42, temperature_buffer, sizeof(temperature_buffer));
+  
+  //Wait one second until we read and send more temperature data
   delay(1000);
 }
