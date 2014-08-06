55d185cffa275998b69b687a2c75ac8acb2b93ae
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-08-06 15:18:36 +0900

    Serial reader now uploads temperature to specified host

--- scan_serial.py
+++ scan_serial.py
@@ -10,7 +10,7 @@ from threading import Thread, Event
 from logging import Logger, Formatter, StreamHandler
 from logging.handlers import RotatingFileHandler
 from argparse import ArgumentParser
-
+import json
 
 logger = Logger(__name__)
 logger.setLevel('DEBUG')
@@ -26,7 +26,8 @@ parser = ArgumentParser()
 parser.add_argument('-r', '--remote-host', help = 'Server IP address e.g., 107.170.251.142')
 parser.add_argument('-b', '--baud', help = 'Serial port baud rate (default 57600)', default = 57600)
 parser.add_argument('-q', '--queue_size', help = 'The size of the queue that functions as a buffer between Serial-to-Internet', default = 100)
-parser.add_argument('-u', '--upload_interval', help = 'Interval in seconds (can be float) between uploading to the server', default = 100)
+parser.add_argument('-u', '--upload_interval', help = 'Interval in seconds (can be float) between uploading to the server', default = 1)
+parser.add_argument('-p', '--port', help = 'Port on the server', default = 8080)
 
 args = parser.parse_args()
 
@@ -77,38 +78,43 @@ def read_serial(name, is_running):
 
 
 def upload_daemon(name, is_running):
+
 	logger.debug('Running %s daemon'%name)
 	while is_running.isSet():
 		try:
-			request = urllib2.Request(url, data = color, headers = {'Content-Type':'text/plain'})
+			data = get_data_in_queue()
+			logger.debug(json.dumps(data))
+			request = urllib2.Request(url, data = json.dumps(data), headers = {'Content-Type':'text/plain'})
 			response = urllib2.urlopen(request)
+			logger.debug(response)
 		except Empty:
 			pass
+		time.sleep(args.upload_interval)
+
+def clean_reading(reading):
+	return reading.replace('\n', '').replace('\r', '')
 
 def get_data_in_queue():
 	data_list = list()
 	while not queue.empty():
-		data_list.append(queue.get())
+		data_list.append( clean_reading(queue.get()))
 	return data_list
 
 if __name__ == "__main__":
 	logger.info('*******************************************************\n')
 	logger.info('Process id: %s', os.getpid())
 	
-
-
-
 	is_running = Event()
 	is_running.set()
 
 	
 	serial_reader = Thread(target = read_serial, args = ('Serial reader', is_running))
 	serial_reader.start()
-	print args.remote_host
+
 
 	if args.remote_host:
-		url = "http://%s/color"%args.remote_host
-		data_sender = Thread(target = send_color, args = ('Data sender', is_running))
+		url = "http://%s:%s/update_temperature"%(args.remote_host, args.port)
+		data_sender = Thread(target = upload_daemon, args = ('Data sender', is_running))
 		data_sender.start()
 	else:
 		print 'ATTENTION: Running without remote host, so no data is being sent to server'
--- webapp/app/views.py
+++ webapp/app/views.py
@@ -5,11 +5,11 @@ from flask import render_template, request
 def static_wall():
 	return render_template('index.html')
 
-@flapp.route('/color', methods = ['GET', 'POST'])
-def send_color():
-	color = request.data
-	print color
-	socketio.emit('new serial data', {'color':color}, namespace = '/serial')
+@flapp.route('/update_temperature', methods = ['GET', 'POST'])
+def store_temperature():
+	temperature = request.data
+	print temperature
+	socketio.emit('new serial data', {'color':temperature}, namespace = '/serial')
 	return 'OK'
 
 @socketio.on('request serial data', namespace = '/serial')
