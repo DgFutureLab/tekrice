6b4e92512c9d0d7b4d823575d03f7cef529b09db
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-08-07 13:22:32 +0900

    Renames a few functions

--- REST_api/app/views.py
+++ REST_api/app/views.py
@@ -8,13 +8,13 @@ def static_wall():
 	return render_template('index.html')
 
 @flapp.route('/update_temperature', methods = ['GET', 'POST'])
-def store_temperature():
+def emit_temperature():
 	socketio.emit('new serial data', {'temperature': format_data(json.loads(request.data))}, namespace = '/serial')
 	return 'OK'
 
 @socketio.on('request serial data', namespace = '/serial')
-def send_images():
-	flapp.logger.debug('Got images request in namespace: grid')
+def respond_to_data_requiest():
+	flapp.logger.debug('Got request for data')
 
 def format_data(sensor_data):
 
