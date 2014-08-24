5c7ffaa896602359ae73664294b03a1f0870d67e
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-08-24 17:43:47 +0900

    Creates basic logging system

--- REST_api/app/loggers.py
+++ REST_api/app/loggers.py
@@ -4,26 +4,21 @@ import os
 from sys import stdout
 from app import flapp
 
-flapp.logger.setLevel('DEBUG')
+# flapp.logger.setLevel('DEBUG')
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 filehandler = logging.FileHandler('api.log')
-filehandler.setLevel('DEBUG')
+filehandler.setLevel(flapp.config['LOGLEVEL'])
 filehandler.setFormatter(formatter)
 flapp.logger.addHandler(filehandler)
 
 
+consolehandler = logging.StreamHandler(stdout)
+consolehandler.setFormatter(formatter)
+consolehandler.setLevel(flapp.config['LOGLEVEL'])
+flapp.logger.addHandler(consolehandler)
 
-
-
-# consolehandler = logging.StreamHandler(stdout)
-# consolehandler.setFormatter(formatter)
-# consolehandler.setLevel('WARNING')
-# logger.addHandler(consolehandler)
-
-
-### Create logger for database events
 
 # traffic_logger = logging.Logger(name = 'api.traffic')
 # traffic_logger.setLevel('INFO')
@@ -33,10 +28,3 @@ flapp.logger.addHandler(filehandler)
 # database_logger.setLevel('INFO')
 # database_logger.addFilter(logging.Filter('api.db'))
 # database_logger.addHandler(consolehandler)
-
-
-
-# traffic_logger.addHandler(streamhandler)
-# api_logger.addHandler(streamhandler)
-
-	# handler = logging.FileHandler('f', encoding = 'utf8')
