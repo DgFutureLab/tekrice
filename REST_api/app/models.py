b4c045bc2e0439814e1bbba8d8a720e214fd1be2
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-08-24 17:44:51 +0900

    db session is now rolled back automatically when inserting object that causes a db integrity error

--- REST_api/app/models.py
+++ REST_api/app/models.py
@@ -1,5 +1,6 @@
 from database import Base, db_session
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import relationship
 import uuid
 from collections import Iterable
@@ -13,12 +14,18 @@ class ExpectedFieldException(Exception):
 
 
 def create(model):						### 'create' is the name of the decorator
-	@staticmethod	
+	@staticmethod
 	def autocommit(*args, **kwargs): 		### Gets arguments for the object to create
 		instance = model(*args, **kwargs) 	### Instance is created
-		db_session.add(instance)			### ..added to the seesion
-		db_session.commit()					### ..inserted to the database
-		return instance 					### ..and returned to the caller
+		try:
+			db_session.add(instance)			### ..added to the seesion
+			db_session.commit()					### ..inserted to the database
+			return instance 					### ..and returned to the caller
+		except IntegrityError, e:
+			flapp.logger.exception(e.message)
+			db_session.rollback()
+			flapp.logger.warning('Rolled back session')
+		
 	model.create = autocommit 			###	The model class (e.g. Node, Sensor, etc, is given a create method which calls the inner function) 		
 	return model						### The decorated model class is returned and replaces the origin model class
 
@@ -55,7 +62,7 @@ class Node(Base):
 		return {'type': '<Node>', 'uuid': self.uuid, 'alias':self.alias, 'sensors': map(lambda s: s.json_summary(), self.sensors)}
 
 	def json_summary(self):
-		return {'type': '<Node>', 'uuid': self.uuid, 'alias':self.alias}
+		return {'type': '<Node>', 'uuid': self.uuid, 'alias':self.alias, 'number of sensors': len(self.sensors)}
 	
 	def __repr__(self):
 		return json.dumps(self.json_summary())
@@ -142,10 +149,10 @@ class Sensor(Base):
 	# 	return '<Sensor> uuid: %s, alias: %s, node: %s, sensor_type: %s, readings: %s'%(self.uuid, self.alias, self.node, self.sensortype, self.readings)
 
 	def json_detailed(self):
-		return {'type': '<Sensor>', 'uuid': self.uuid, 'sensor_type':self.sensortype, 'readings': map(lambda r: r.json_summary(), self.readings)}
+		return {'type': '<Sensor>', 'uuid': self.uuid, 'sensor_type':self.sensortype, 'readings': map(lambda r: r.json_detailed(), self.readings)}
 
 	def json_summary(self):
-		return {'type': '<Sensor>', 'uuid': self.uuid, 'sensor_type':self.sensortype}
+		return {'type': '<Sensor>', 'uuid': self.uuid, 'sensor_type':self.sensortype, 'readings': map(lambda r: r.json_summary(), self.readings)}
 		# return {'type': '<Sensor>', 'uuid': self.uuid, 'node':self.node, 'sensor_type':self.sensortype, 'readings': map(lambda r: r.details(), self.readings)}
 
  
