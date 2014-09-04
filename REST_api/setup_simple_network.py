75a10ea6413f2049328d8868dacdb2a2372de298
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-09-04 18:16:52 +0900

    Adds script for setting up simple system with two nodes

---
+++ REST_api/setup_simple_network.py
@@ -0,0 +1,22 @@
+import satoyama
+from satoyama.models import Node, Sensor, SensorType, Reading
+
+if __name__ == "__main__":
+	satoyama.database.recreate()
+	
+	collector1 = Node.create(alias = "chibi_temp_dist")
+	collector2 = Node.create(alias = 'chibi_temp')
+	st_temp = SensorType('temperature', 'C')
+	st_dist = SensorType('distance', 'cm')
+	st_hum = SensorType('humidity', '%')
+
+	Sensor.create(sensortype = st_temp, node = collector1, alias = 'temperature')
+	Sensor.create(sensortype = st_dist, node = collector1, alias = 'distance')
+	Sensor.create(sensortype = st_dist, node = collector1, alias = 'humidity')
+
+	Sensor.create(sensortype = st_temp, node = collector2, alias = 'temperature')
+	Sensor.create(sensortype = st_dist, node = collector2, alias = 'distance')
+	Sensor.create(sensortype = st_dist, node = collector2, alias = 'humidity')
+
+	print '****** Nodes: ', Node.query.all()
+	print '****** Sensors: ', Sensor.query.all()
