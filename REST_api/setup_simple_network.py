8c499599194e6701a757837842ef49437472f4d4
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-09-10 17:18:01 +0900

    Adds saboten node

--- REST_api/setup_simple_network.py
+++ REST_api/setup_simple_network.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
 	
 	collector1 = Node.create(alias = "chibi_temp_dist")
 	collector2 = Node.create(alias = 'chibi_temp')
+	collector3 = Node.create(alias = 'saboten_hector_desk')
+
 	st_temp = SensorType('temperature', 'C')
 	st_dist = SensorType('distance', 'cm')
 	st_hum = SensorType('humidity', '%')
@@ -17,6 +19,8 @@ if __name__ == "__main__":
 	Sensor.create(sensortype = st_temp, node = collector2, alias = 'temperature')
 	Sensor.create(sensortype = st_dist, node = collector2, alias = 'distance')
 	Sensor.create(sensortype = st_dist, node = collector2, alias = 'humidity')
+
+	Sensor.create(sensortype = st_dist, node = collector3, alias = 'distance')
 
 	print '****** Nodes: ', Node.query.all()
 	print '****** Sensors: ', Sensor.query.all()
