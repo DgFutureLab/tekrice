3c4a41b03f7e6890e5887d3f8c9d6f10fa89ba00
Hector Garcia <hectorgarcia@gmail.com>
2014-09-10 16:31:59 +0900

    Sonar code in a C library

--- collection/chibi_collector/chibi_collector.ino
+++ collection/chibi_collector/chibi_collector.ino
@@ -1,3 +1,5 @@
+#include <NewPing.h>
+
 #include <config.h>
 #include <utilsawesome.h>
 
@@ -10,21 +12,21 @@ the aggregator as defined in "config.h"
 
 #include <chibi.h>
 #include "DHT.h"
-
-/*
-Saboten includes
+  
 #include <avr/sleep.h>
 #include <avr/power.h>
 #include <avr/wdt.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SdFat.h>
-#include <pcf2127.h>*/
+#include <pcf2127.h>
+#include <NewPing.h>
 
 #define DHTTYPE DHT11   // Type of DHT sensor, in our case we are using DHT11
 #define DHT11_PIN A0    // Pin where the DHT11 is connected
 
 dht DHT;
+NewPing sonar(SONAR_PIN,SONAR_PIN,200);
 
 /**************************************************************************/
 // Initialize
@@ -68,7 +70,8 @@ void loop()
   long duration, inches, cm;
   
   // Read sonar distance
-  float distance = sonar_measure_distance();
+  float distance = sonar.ping() / US_ROUNDTRIP_CM; 
+  
   if (distance > 0) {
     Reading dist = {"distance", distance, millis()};
     add_to_tx_buf((char*)tx_buf, &dist);
@@ -92,56 +95,3 @@ void loop()
   //Wait
   delay(1000);
 }
-
-float sonar_measure_distance(){
-  int checkTimes = 10;
-  long duration, cm;
-
-  // read sensor data and send average data
-  int sum = 0;
-  for (int i=0; i<checkTimes; i++){
-    delay(100);
-    // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-    // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-    pinMode(SONAR_PIN, OUTPUT);
-    digitalWrite(SONAR_PIN, LOW);
-    delayMicroseconds(2);
-    digitalWrite(SONAR_PIN, HIGH);
-    delayMicroseconds(5);
-    digitalWrite(SONAR_PIN, LOW);
-  
-    // The same pin is used to read the signal from the PING))): a HIGH
-    // pulse whose duration is the time (in microseconds) from the sending
-    // of the ping to the reception of its echo off of an object.
-    pinMode(SONAR_PIN, INPUT);
-    duration = pulseIn(SONAR_PIN, HIGH);
-  
-    // convert the time into a distance
-    cm = microsecondsToCentimeters(duration);
-    if(VERBOSE == 1){ 
-      Serial.print(i);
-      Serial.print(cm, 1);
-      Serial.print("cm");
-      Serial.println();
-    }
-    
-    sum += cm;
-  }
-  float average = sum/(float)checkTimes;
-  if(VERBOSE == 1){
-    Serial.print("Average: ");
-    Serial.println(average, 1);
-    Serial.println();
-  }
-  return average;
-}
-
-long microsecondsToCentimeters(long microseconds)
-{
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the
-  // object we take half of the distance travelled.
-  return microseconds / 29 / 2;
-}
-
-
