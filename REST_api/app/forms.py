56aa3dd3c42e81e57c02b002b4fba82238fdbddb
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-09-12 13:47:41 +0900

    Moved API files to different repo

--- REST_api/app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class NodeForm(Form):
-	uuid = StringField('uuid')
-	alias = StringField('alias')
--- REST_api/run_tests.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from tests.db import *
-from app import conf, flapp
-
-if __name__ == "__main__":
-	conf.config_test_env(flapp)
-	unittest.main()
